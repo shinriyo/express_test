c73c3637a921ca1d59f3bf00e94111bb326b3d47
shinriyo <shinriyo@gmail.comm>
2014-02-13 23:53:54 +0900

    form new

--- blog/coffee/app.coffee
+++ blog/coffee/app.coffee
@@ -17,7 +17,7 @@ app.use(app.router)
 
 # routing
 app.get('/', post.index)
-app.get('/posts/:id', post.show)
+app.get('/posts/:id(0-9]+)', post.show)
 app.get('/posts/new', post.new)
 ###
 app.post('/posts/create', post.create)
--- blog/js/app.js
+++ blog/js/app.js
@@ -26,17 +26,17 @@ app.use(app.router);
 
 app.get('/', post.index);
 
-app.get('/posts/:id', post.show);
+app.get('/posts/:id(0-9]+)', post.show);
 
 app.get('/posts/new', post["new"]);
-
 
 /*
 app.post('/posts/create', post.create)
 app.get('/posts/:id/edit', post.edit)
 app.put('/posts/:id/', post.update)
 app.delete('/posts/:id/', post.destroy)
- */
+*/
+
 
 app.listen(3000);
 
